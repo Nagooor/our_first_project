1a94f13a3be0ebdbe832185630d990777560099d
Nagoor <princenagoormaahi@gmail.com>
2023-11-07 21:50:19 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse 
+
+def NagoorBabu(request):
+    return HttpResponse('Hii NagoorBabu how are you')
